7c5f2f3a7518cc1f7dbc00439295195876f6723c
keriangouriou <kerian.gouriou@gmail.com>
2025-10-28 17:37:21 +0100

    Dialogue Box N°1

---
+++ Assets/Scripts/WriteDialogue.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+public class WriteDialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textDialogue;
+    public string[] lines;
+    public float textSpeed;
+
+    private int index;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        textDialogue.text = string.Empty;
+        StartDialogue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (textDialogue.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textDialogue.text = lines[index];
+            }
+        }
+
+    }
+
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textDialogue.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if(index < lines.Length - 1)
+        {
+            index++;
+            textDialogue.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
