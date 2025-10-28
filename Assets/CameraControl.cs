09f609dead20fff8b3068b52c29908b857bbf56e
keriangouriou <kerian.gouriou@gmail.com>
2025-10-28 14:05:01 +0100

    Camera première personne

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,33 @@
+using JetBrains.Annotations;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraControl : MonoBehaviour
+{
+    public float sensX;
+    public float sensY;
+
+    public float yRotation;
+    public float xRotation;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // Get X and Y mouse inputs
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        yRotation = Mathf.Clamp(yRotation, -10f, 10f);
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+    }
+}
