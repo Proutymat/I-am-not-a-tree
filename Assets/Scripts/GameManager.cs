4f23e7a677920fead324c0d4346e18b43a72b88e
Matéo Avventuriero <mateo.avventuriero@gmail.com>
2025-10-29 01:01:05 +0100

    fadein

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,34 +1,33 @@
+using System.Collections;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
 
     [SerializeField] private int timeToWait = 7;
     [SerializeField] private GameObject endingObject;
-    [SerializeField] private GameObject character;
+    [SerializeField] private Image character;
     private int eventCount = 0;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // On le rend totalement transparent au départ
+        var color = character.color;
+        color.a = 0f;
+        character.color = color;
+
         
+        StartEvent();
     }
 
-    void StartEvent()
+    private void StartEvent()
     {
-        // On récupère le matériau
-        var renderer = character.GetComponent<Renderer>();
-        if (renderer != null)
-        {
-            // On met l'alpha à 0 au départ
-            Color color = renderer.material.color;
-            color.a = 0f;
-            renderer.material.color = color;
-
-            // On fait apparaître progressivement sur "timeToWait" secondes
-            renderer.material.DOFade(1f, timeToWait).SetEase(Ease.InOutSine);
-        }
+        Debug.Log("COUCOU");
+        // On fait apparaître progressivement sur "timeToWait" secondes
+        character.DOFade(1f, timeToWait).SetEase(Ease.InOutSine);
     }
 
     private void PlayEnding()
